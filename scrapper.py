c8454222d799563d3b6573ed81277317661103a7
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-01-18 10:42:10 +0530

    Exact text rendered out

--- scrapper.py
+++ scrapper.py
@@ -13,11 +13,10 @@ def gettheNews(newsLink):
     try:
             newsArticle=requests.get(newsLink)
             soup=bs4.BeautifulSoup(newsArticle.text)
-            article=soup.select('p.body')
-            #article=re.findall(r'<div class="article-text">(.*?)</div>',newsArticle.text)
-            print newsLink
-            for single in article:
-                print re.findall(r'<p class="body">(.*?)</p>',single)
+            articlePiece=soup.findAll('p',{"class":"body"})
+            for article in articlePiece:
+                print article.renderContents()
+
 
     except Exception,e:
        print str(e)
@@ -25,7 +24,6 @@ def main():
         page = 'http://www.thehindu.com/navigation/?type=rss'
         response = requests.get(page)
         soup = bs4.BeautifulSoup(response.text)
-        #print soup
         links = soup.select('div.sitemap ul li a[href^=]')
         for link in links:
             toOpen=link.attrs.get('href')
