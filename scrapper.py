787729b3a67b97f76023d00a430dcb539c319017
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-01-23 23:48:24 +0530

    Sha generated

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,7 @@ import bs4
 import requests
 import feedparser
 import json
+import hashlib
 def parseRSS(rssURL):
             indivdualPage=feedparser.parse(rssURL);
             for inds in indivdualPage.entries:
@@ -17,15 +18,17 @@ def gettheNews(newsLink,date):
             soup=bs4.BeautifulSoup(newsArticle.text)
             articlePieces=soup.find_all('p',"body") 
             for article in articlePieces:
-                contents+=article.get_text()
+                contents+=(article.get_text()).encode('utf8')
             if contents=="":
                 print "no content"
             else:
-                print contents
-                print date
+               getSha1(contents,date)
     except Exception,e:
        print str(e)
-
+def getSha1(contents,date):
+    print hashlib.sha1(contents).hexdigest()
+    print contents
+    print date
 def main():
         page = 'http://www.thehindu.com/navigation/?type=rss'
         response = requests.get(page)
