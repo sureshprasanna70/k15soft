7245cf50a619318499d29f4dfa8602e582de5dcf
sureshprasanna70 <sureshprasanna70@gmail.com>
2015-01-26 15:54:25 +0530

    Exception handling

--- scrapper.py
+++ scrapper.py
@@ -6,56 +6,73 @@ import hashlib
 import urllib
 from dateutil.parser import parse
 def parseRSS(rssURL):
-            indivdualPage=feedparser.parse(rssURL);
-            for inds in indivdualPage.entries:
+            indivdualpage=feedparser.parse(rssURL);
+            for inds in indivdualpage.entries:
                 date=inds.published
                 link=inds.links[0]['href']
                 if link != "http://www.thehindu.com/":
-                    gettheNews(link,date)
-def gettheNews(newsLink,date):
+                    getthenews(link,date)
+def getthenews(newslink,date):
     try:
             contents="";
             blacklist=["b","i","u"]
-            newsArticle=requests.get(newsLink)
-            soup=bs4.BeautifulSoup(newsArticle.text)
-            articlePieces=soup.find_all('p',"body") 
-            for article in articlePieces:
+            newsarticle=requests.get(newslink)
+            soup=bs4.BeautifulSoup(newsarticle.text)
+            articlepieces=soup.find_all('p',"body") 
+            for article in articlepieces:
                 contents+=(article.get_text()).encode('utf8')
             if contents=="":
                 print "no content"
             else:
-               getTaxonomy(contents,date)
+               get_taxonomy(contents,date,newslink)
     except Exception,e:
-       print str(e)
-def getTaxonomy(contents,date):
+       print "News fails"
+def get_taxonomy(contents,date,newslink):
     yahoourl='https://query.yahooapis.com/v1/public/yql?q='
     yahooquery='select * from contentanalysis.analyze where text="'
     dupcontents=contents
     querywithtext=yahooquery+" ".join(dupcontents.split())+'"'
     urlwithquery=yahoourl+querywithtext+"&format=json"
     taxo=requests.get(urlwithquery)
-    taxojson=json.loads(taxo.text)
-    for each in taxojson:
-        entitiesarray=taxojson['query']['results']['entities']['entity']
-        for entities in entitiesarray:
-            print entities['score']
-            print entities['text']['content']
-            if "wiki_url" in entities:
-                print entities['wiki_url']
-    pubdate=parse(date).date()
-    date=pubdate.strftime("%Y-%m-%d")
+    try:
+        taxojson=json.loads(taxo.text)
+        pubdate=parse(date).date()
+        date=pubdate.strftime("%Y-%m-%d")
+        for each in taxojson:
+            entitiesarray=taxojson['query']['results']['entities']['entity']
+            for entities in entitiesarray:
+                taxonomy={}
+                taxonomy['score']=entities['score']
+                taxonomy['name']=entities['text']['content']
+                if "wiki_url" in entities:
+                    try:
+                        taxonomy['url']=entities['wiki_url']
+                    except Exception,e:
+                        taxonomy['url']='nil'
+                if 'types' in entities:
+                    try:
+                        taxonomy['ent_type']=entities['types']['type']['content']
+                    except Exception,e:
+                        taxonomy['ent_type']='nil'
+
+        sendtoes(hashlib.sha1(contents).hexdigest(),contents,date,taxonomy,newslink)
+    except Exception,e:
+        print "YQL fails"
+def sendtoes(index,contents,date,taxonomy,link):
     print date
-    #print datetime.strptime(pubdate,'%Y%m%d')
-    #print hashlib.sha1(contents).hexdigest()
-    #print contents
-    #print date
+    print link
+    print taxonomy
+'''
+Sample json doc for es
+{"news": {"url":"http://www.google.com","article":"It is great","published":"2015-01-25","taxonomy":{"entity1": {"name": "Obama","score":0.1},"entity2": { "name": "Michelle","score": 0}}}}
+'''
 def main():
         page = 'http://www.thehindu.com/navigation/?type=rss'
         response = requests.get(page)
         soup = bs4.BeautifulSoup(response.text)
         links = soup.select('div.sitemap ul li a[href^=]')
         for link in links:
-            toOpen=link.attrs.get('href')
-            parseRSS(toOpen)
+            toopen=link.attrs.get('href')
+            parseRSS(toopen)
 
 main()
